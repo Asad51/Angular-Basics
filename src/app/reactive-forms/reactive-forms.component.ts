cfd54c0466574bed3f7bad7fa567e5abf2064bcd
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-10-17 01:33:22 +0600

    Reactive form validation

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
+import { forbiddenNameValidator } from "./shared/user-name.validator";
+import { passwordValidator } from "./shared/password.validator";
 
 @Component({
     selector: "app-reactive-forms",
@@ -8,31 +10,46 @@ import { FormGroup, FormControl } from "@angular/forms";
 })
 export class ReactiveFormsComponent implements OnInit {
 
-    registrationForm = new FormGroup({
-        username: new FormControl("Asad"),
-        password: new FormControl(''),
-        confirmPassword: new FormControl(''),
-        address: new FormGroup({
-            city: new FormControl(''),
-            state: new FormControl(''),
-            country: new FormControl('')
-        })
-    });
+    // registrationForm = new FormGroup({
+    //     username: new FormControl(""),
+    //     password: new FormControl(''),
+    //     confirmPassword: new FormControl(''),
+    //     address: new FormGroup({
+    //         city: new FormControl(''),
+    //         state: new FormControl(''),
+    //         country: new FormControl('')
+    //     })
+    // });
 
-    constructor() {}
+    get userName(){
+        return this.registrationForm.get("username");
+    }
+
+    constructor( private fb: FormBuilder) {}
+
+    registrationForm = this.fb.group({
+        username: ["", [Validators.required, Validators.minLength(4), forbiddenNameValidator(/admin/)]],
+        password: ['', Validators.required],
+        confirmPassword: [''],
+        address: this.fb.group({
+            city: [''],
+            state: [''],
+            country: ['Bangladesh']
+        })
+    }, {Validator: passwordValidator} );
 
     ngOnInit() {}
 
     loadApiData(){
-        this.registrationForm.setValue({
-            username: "Asad",
-            password: "test",
-            confirmPassword: "test",
-            address: {
-                city: "Binodpur",
-                state: "Rajshahi",
-                country: "Bangladesh"
-            }
-        })
+        // this.registrationForm.setValue({
+        //     username: "Asad",
+        //     password: "test",
+        //     confirmPassword: "test",
+        //     address: {
+        //         city: "Binodpur",
+        //         state: "Rajshahi",
+        //         country: "Bangladesh"
+        //     }
+        // })
     }
 }
